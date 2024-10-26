6ac58b8cbfa6ae046153bd827d9b832af20ac55b
saivinay2223 <saivinaykumarbikki@gmail.com>
2024-10-25 20:04:40 -0500

    updated users.js code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+// Initialize value and total
+let value = 0;
+let total = 1;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value += 2;       // Increment value by 2
+  total += value;   // Add value to total
+  res.send(`Total is: ${total}`);  // Send response with the updated total
 });
 
 module.exports = router;
