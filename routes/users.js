cb3adff8a9f2e12adf3dd92b20d63695295bce74
saivinay2223 <saivinaykumarbikki@gmail.com>
2024-10-30 11:48:47 -0500

    updated code of users with multiplication to total

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
-// Initialize value and total
 let value = 0;
 let total = 1;
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  value += 2;       // Increment value by 2
-  total += value;   // Add value to total
-  res.send(`Total is: ${total}`);  // Send response with the updated total
+  value += 2;       
+  total += value * 2; // Multiply value by 2 before adding to total
+  res.send(`Total is: ${total}`);  
 });
 
 module.exports = router;
