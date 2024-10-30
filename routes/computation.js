d8ab6b1a3479eeaf089bdd482a2363699da2cf3c
saivinay2223 <saivinaykumarbikki@gmail.com>
2024-10-30 11:36:44 -0500

    added computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 router.get('/', (req, res) => {
-  // Randomly select `x` in a range suitable for each function
   const xValues = {
-    cos: Math.random() * Math.PI * 2, // Random value in [0, 2π] for Math.cos
-    asin: Math.random() * 2 - 1, // Random value in [-1, 1] for Math.asin
-    asinh: Math.random() * 10 - 5 // Random value in [-5, 5] for Math.asinh
+    cos: Math.random() * Math.PI * 2, 
+    asin: Math.random() * 2 - 1, 
+    asinh: Math.random() * 10 - 5 
   };
-
-  // List of functions with corresponding random `x` ranges
   const functions = ['cos', 'asin', 'asinh'];
   const selectedFunction = functions[Math.floor(Math.random() * functions.length)];
-
-  // Set `x` based on the selected function
   const x = xValues[selectedFunction];
-
-  // Apply the selected function to `x`
   let y;
   switch (selectedFunction) {
     case 'cos':
@@ -29,9 +21,6 @@ router.get('/', (req, res) => {
       y = Math.asinh(x);
       break;
   }
-
-  // Send the result back
   res.send(`Math.${selectedFunction} applied to ${x} is ${y}`);
 });
-
 module.exports = router;
